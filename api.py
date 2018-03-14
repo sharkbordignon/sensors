93c98710808b1d2130aadb3a6169e35de10381a1
thiago <thiagobordignon@gmail.com>
2018-03-14 00:16:53 +0000

    TBS000: add first endpoint: get temperatures

---
+++ api.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import lib.ds18b20 as ds18b20
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    sensors = ds18b20.sensor()
+    sensors_temp = []
+    for sensor in sensors:
+        if ds18b20.read(sensor) != None:
+            sensors_temp.append(ds18b20.read(sensor)[0])
+    ds18b20.kill()
+    return sensors_temp
+    #return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
