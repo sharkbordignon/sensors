fb250fe5cee44d332affbfa6c4c6d5bc88e7195f
thiago <thiagobordignon@gmail.com>
2018-03-26 02:46:29 +0100

    TBS000: Add individual sensor reading endpoint

--- api.py
+++ api.py
@@ -15,6 +15,24 @@ def index():
     #return sensors_temp
     #return "Hello, World!"
 
+@app.route('/inside')
+def inside():
+    sensor = getInsideSensor()
+    if ds18b20.read(sensors) != None:
+        return "Temperature: " + str(ds18b20.read(sensor)[0])
+
+@app.route('/outside')
+def outside():
+    sensor = getOutsideSensor()
+    if ds18b20.read(sensors) != None:
+        return "Temperature: " + str(ds18b20.read(sensor)[0])
+
+def getInsideSensor():
+    return sensors[0]
+
+def getOutsideSensor():
+    return sensors[1]
+
 if __name__ == '__main__':
     sensors = ds18b20.sensor()
     app.run(host='0.0.0.0', port='2121')
