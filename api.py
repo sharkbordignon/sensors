b3edade9ceb17d1f4126186997443980965fd383
thiago <thiagobordignon@gmail.com>
2018-03-26 02:35:22 +0100

    TBS000: remove times, sensor take almost 2 secs to read :/

--- api.py
+++ api.py
@@ -1,18 +1,13 @@
 from flask import Flask
 import lib.ds18b20 as ds18b20
-import time
-
 
 app = Flask(__name__)
 sensors = None
 
 @app.route('/')
 def index():
-    print("READ")
-    print(time.time())
     sensors_temp = []
     for sensor in sensors:
-        print(time.time())
         if ds18b20.read(sensor) != None:
             sensors_temp.append(ds18b20.read(sensor)[0])
     #ds18b20.kill()
@@ -21,8 +16,5 @@ def index():
     #return "Hello, World!"
 
 if __name__ == '__main__':
-    print("INITIALIZE")
-    print(time.time())
     sensors = ds18b20.sensor()
-    print(time.time())
     app.run(host='0.0.0.0', port='2121')
