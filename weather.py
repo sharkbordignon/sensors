cdace775292a20fcf9df00fb8d649451f5224e5d
thiago <thiagobordignon@gmail.com>
2018-04-14 17:00:15 +0100

    Quick add to spreadsheet

--- weather.py
+++ weather.py
@@ -3,6 +3,7 @@ import time
 import datetime
 import saveFile
 import emailWrapper
+import spreadsheet
 
 import lib.ds18b20 as ds18b20
 import lib.rain as rain
@@ -13,9 +14,12 @@ if __name__ == '__main__':
         sensors = ds18b20.sensor()
         while True:
             sensors_temp = []
+            i = 0
             for sensor in sensors:
                 if ds18b20.read(sensor) != None:
                     sensors_temp.append(ds18b20.read(sensor)[0])
+                    sensor_name = 'inside' if 1 == 0 else 'outside'
+                    spreadsheet.insertRow(sensor_name, datetime.datetime.now(), ds18b20.read(sensor)[0])
                     #print sensor
                     #print "Current temperature : %0.3f C" % ds18b20.read(sensor)[0]
                     #print "Current temperature : %0.3f F" % ds18b20.read(sensor)[1]
